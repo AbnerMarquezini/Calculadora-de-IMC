18dcb9cbd93a1c94f85b021d681315179154ba14
AbnerMarquezini <contatoabnermarquezini@gmail.com>
2022-07-21 11:51:27 -0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -6,4 +6,26 @@ function calcularImc(){
     var pos = document.getElementById('pos');
 
     res.textContent = `O seu IMC é ${imc}`;
+
+    let classificar = '';
+
+    if(imc>=40){
+        classificar = "Você está classificado em 'Obesidade III'.";
+        pos.textContent = classificar;
+    } else if(imc<40 && imc>=35){
+        classificar = "Você está classificado em 'Obesidade II'.";
+        pos.textContent = classificar;
+    } else if(imc<35 && imc>=30){
+        classificar = "Você está classificado em 'Obesidade I'.";
+        pos.textContent = classificar;
+    } else if(imc<30 && imc>=25){
+        classificar = "Você está classificado em 'Sobrepeso'.";
+        pos.textContent = classificar;
+    } else if(imc<25 && imc>=18.6){
+        classificar = "Você está classificado em 'Peso Normal'.";
+        pos.textContent = classificar;
+    } else {
+        classificar = "Você está classificado em 'Abaixo do peso'.";
+        pos.textContent = classificar;
+    }
 }
