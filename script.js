0b3126a55b64f53dd81474bdaa26a9d2f8680f52
AbnerMarquezini <marqueziniabner@gmail.com>
2023-01-19 18:17:51 -0300

    modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 function calcularImc(){
     var altura = document.getElementById('altura').value;
     var peso = document.getElementById('peso').value;
-    var imc = (peso / ((altura/100) * (altura/100))).toFixed(2);
     var res = document.getElementById('res');
     var pos = document.getElementById('pos');
-
-    res.textContent = `O seu IMC é ${imc}`;
-
-    let classificar = '';
-
-    if(imc>=40){
-        classificar = "Você está classificado em Obesidade III.";
-        pos.textContent = classificar;
-    } else if(imc<40 && imc>=35){
-        classificar = "Você está classificado em Obesidade II.";
-        pos.textContent = classificar;
-    } else if(imc<35 && imc>=30){
-        classificar = "Você está classificado em Obesidade I.";
-        pos.textContent = classificar;
-    } else if(imc<30 && imc>=25){
-        classificar = "Você está classificado em Sobrepeso.";
-        pos.textContent = classificar;
-    } else if(imc<25 && imc>=18.6){
-        classificar = "Você está classificado em Peso Normal.";
-        pos.textContent = classificar;
+    if (altura, peso == 0 ) {
+        res.textContent = 'Insira os dados necessários.';
+        pos.textContent = '';
     } else {
-        classificar = "Você está classificado em Abaixo do peso.";
-        pos.textContent = classificar;
+        var imc = (peso / ((altura/100) * (altura/100))).toFixed(2);
+
+        res.textContent = `O seu IMC é ${imc}`;
+
+        let classificar = '';
+
+        if(imc>=40){
+            classificar = "Você está classificado em Obesidade III.";
+            pos.textContent = classificar;
+        } else if(imc<40 && imc>=35){
+            classificar = "Você está classificado em Obesidade II.";
+           pos.textContent = classificar;
+        } else if(imc<35 && imc>=30){
+            classificar = "Você está classificado em Obesidade I.";
+            pos.textContent = classificar;
+        } else if(imc<30 && imc>=25){
+            classificar = "Você está classificado em Sobrepeso.";
+            pos.textContent = classificar;
+        } else if(imc<25 && imc>=18.6){
+            classificar = "Você está classificado em Peso Normal.";
+            pos.textContent = classificar;
+        } else {
+           classificar = "Você está classificado em Abaixo do peso.";
+            pos.textContent = classificar;
+        }
     }
 }
 
@@ -35,4 +40,6 @@ function limpar() {
     var peso = document.getElementById('peso');
     altura.value = '';
     peso.value = '';
+    res.textContent = '';
+    pos.textContent = '';
 }
